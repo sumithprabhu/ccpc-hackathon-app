89c9a8c258371c0dd88e96d0c8f522bec76d727d
Abbas Pathan <pathan.abbas10@gmail.com>
2022-02-11 19:29:33 +0530

    About Page added

--- ccpc-frontend-app/src/components/Body/AboutBody.js
+++ ccpc-frontend-app/src/components/Body/AboutBody.js
@@ -1,24 +1,25 @@
 import React from "react";
 import TargetLink from "../../UI/TargetLink";
 import Navbar from "../Navbar/Navbar";
-import classes from './AboutBody.module.css'
+import classes from "./AboutBody.module.css";
+import aboutPageImage from "../../assets/aboutpageImage.jpg";
+import Footer from '../Footer/Footer'
 
 const AboutBody = () => {
   return (
     <div>
       <Navbar />
+      <div className={classes.container}>
+        <div className={classes.mainimage}>
+          <img src={aboutPageImage} alt="...." />
+        </div>
 
-      <div className={classes.aboutbody}>
-        <>
-          <main>
-            <h2>Who are we?</h2>
-            <p>That feels like an existential question, don't you think?</p>
-          </main>
-          <nav>
-            <TargetLink toPath="/" name="Home"></TargetLink>
-          </nav>
-        </>
+        <div className={classes.textbody}>
+          <h2>Who are we?</h2>
+          <p>That feels like an existential question, don't you think?</p>
+        </div>
       </div>
+    <Footer />
     </div>
   );
 };
--- ccpc-frontend-app/src/components/Card/card.js
+++ ccpc-frontend-app/src/components/Card/card.js
@@ -2,16 +2,14 @@ import React from "react";
 
 import classes from "../Card/card.module.css";
 
-
 export const Card = (props) => {
   return (
     <div className={classes.cardcontainer}>
-      <img
-        src={props.image}
-        alt=""
-      />
-      <h1>{props.details}</h1>
-      <p>{props.descrip}</p>
+      <img src={props.image} alt="" />
+      <div className={classes.nametag}>
+        <h1>{props.details}</h1>
+        {/* <p>{props.descrip}</p> */}
+      </div>
     </div>
   );
 };
